f68ad1dc5cb90bb0e614629bb5821f3bf09bdc8e
Altun <Altun11@yandex.ru>
2025-07-06 23:11:54 +0300

    Исправить замечания по code style №2

--- src/managers/ManagerSaveException.java
+++ src/managers/ManagerSaveException.java
@@ -1,6 +1,8 @@
 package managers;
 
+@SuppressWarnings({"checkstyle:Indentation", "checkstyle:WhitespaceAround", "checkstyle:MissingJavadocType"})
 public class ManagerSaveException extends RuntimeException{
+    @SuppressWarnings({"checkstyle:Indentation", "checkstyle:MissingJavadocMethod"})
     public ManagerSaveException(String message, Throwable cause) {
         super(message, cause);
     }
